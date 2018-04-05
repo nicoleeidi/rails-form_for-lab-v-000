ae523e50714ca938c53883c453948268e3eeeaf8
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 01:30:22 +0000

    automatically backed up by learn

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,7 @@
 Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-  resources:
-  resources:
+    resources :students, only: [:index, :show, :new, :create, :edit, :update]
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'

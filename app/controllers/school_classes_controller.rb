9e47152b5e19d4c42e5cb2118c294cef11c163da
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 01:41:33 +0000

    automatically backed up by learn

--- app/controllers/school_classes_controller.rb
+++ app/controllers/school_classes_controller.rb
@@ -10,6 +10,7 @@ class SchoolClassesController < ApplicationController
   end
   def create
     @school_class= SchoolClass.new(params.require(:school_class))
+    @school_class.save
     redirect_to school_class_path(@school_class)
   end
   def update

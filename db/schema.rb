d21fd84d0a8c6b0a7bc8f82cccfd36f5a87b9a64
Nicole Eidi <nicoleeidi1@gmail.com>
2018-04-05 02:19:41 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180404230333) do
-
-  create_table "school_class", force: :cascade do |t|
-    t.string  "title"
-    t.integer "room_number"
-  end
+ActiveRecord::Schema.define(version: 1) do
 
   create_table "students", force: :cascade do |t|
     t.string   "first_name"
